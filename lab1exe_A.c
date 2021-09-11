c64c21161444a1ec8a6731a75457006385c0716b
Bhavyai Gupta <zbhavyai@gmail.com>
2021-09-11 16:23:43 -0600

    Lab 1 Exercise A

---
+++ lab1exe_A.c
@@ -0,0 +1,25 @@
+/*
+* File Name: lab1exe_A.c
+* Lab # and Assignment #: Lab 1 Exercise A
+* Lab section: B01
+* Completed by: Bhavyai Gupta
+* Submission Date: NA
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 0, b = 0;
+
+    printf("Please enter a value for variable a:\n");
+    scanf("%d", &a);
+
+    printf("Please enter a value for variable b:\n");
+    scanf("%d", &b);
+
+    printf("The values of a and b are %d for a and %d for b.\n", a, b);
+    printf("The value of a %% b is %d\n", a % b);
+
+    return 0;
+}
