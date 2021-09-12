0042b2a7bc5ab83ce8bb51f78c3ed3df70097975
Bhavyai Gupta <zbhavyai@gmail.com>
2021-09-11 23:21:28 -0600

    Restoring Exercise C

--- lab1exe_C.c
+++ lab1exe_C.c
@@ -16,32 +16,38 @@ int main(void)
     int *fred;
     int *sam;
 
-    /* point one */
     bar = 100;
     foo = 200;
     fred = &foo;
-    printf("point one: foo is %d and bar is %d.\n", foo, bar);
 
-    /* point two */
+    /* point one */
+
+    printf("point one: foo is %d and bar is %d.\n", foo, bar);
     sam = &bar;
     *sam += 30;
     *fred -= 40;
-    printf("point two: foo is %d and bar is %d.\n", foo, bar);
 
-    /* point three */
+    /* point two */
+
+    printf("point two: foo is %d and bar is %d.\n", foo, bar);
     fred = &bar;
     *fred += 5;
+
+    /* point three */
+
     printf("point three: foo is %d and bar is %d.\n", foo, bar);
     printf("point three: *fred is %d and *sam is %d.\n", *fred, *sam);
-
-    /* point four */
     sam = &foo;
     *sam = *fred;
-    printf("point four: foo is %d and bar is %d.\n", foo, bar);
 
-    /* point five */
+    /* point four */
+
+    printf("point four: foo is %d and bar is %d.\n", foo, bar);
     *sam *= 100;
     sam = fred;
+
+    /* point five */
+
     printf("point five: foo is %d and bar is %d.\n", foo, bar);
     printf("point five: *fred is %d and *sam is %d.\n", *fred, *sam);
 
